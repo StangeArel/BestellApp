3f6b7aa79c4e4e44c17ebf16b2c2f5265f058f21
Valeriya Stange <>
2025-01-24 23:14:45 +0100

    renderFunctions

--- script.js
+++ script.js
@@ -52,35 +52,85 @@ let dishes = [
     }
 ];
 
+let basket = [
+    {
+        name: "Pad Thai",
+        amount: 16,
+        price: 12.95
+    },
+    {
+        name: "Steakhouse Ribeye",
+        amount: 7,
+        price: 18.95
+    }
+];
+
 
 function init() {
     renderDishes();
+    renderBasket();
 }
 
 function renderDishes() {
-let dishesRef = document.getElementById("dishes");
-dishesRef.innerHTML = "";
-for (let i = 0; i < dishes.length; i++) {
-    let dish = dishes[i];
-    let template = /*HTML*/`
+    let dishesRef = document.getElementById("dishes");
+    dishesRef.innerHTML = "";
+    for (let i = 0; i < dishes.length; i++) {
+        let dish = dishes[i];
+        let template = `
                         <div class="dish">
                             <div class="dishHeader">
                                 <h2>${dish.name}</h2>
-                                <a class="dishHeaderBtn">
+                                <button onclick="addToBasket(indexDishes)" class="dishHeaderBtn">
                                     <img class="imgPlus" src="./assets/img/plus.png" alt="img. Button Plus">
-                                </a>
+                                </button>
                             </div>
                             <p id="ingredients">${dish.description}</p>
                             <p id="price">${dish.price.toLocaleString(undefined, { //toLocaleString-Funktion ist großartig
-                                            minimumFractionDigits: 2,
-                                            maximumFractionDigits: 2,
-                                            style: "currency",
-                                            currency: "EUR"
-                                        })}
-                                        </p>
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+            style: "currency",
+            currency: "EUR"
+        })}</p>
                         </div>
     `
-    dishesRef.innerHTML += template;
+        dishesRef.innerHTML += template;
+    }
 }
 
+function renderBasket() {
+    let basketRef = document.getElementById("basketSelectedDishes");
+    basketRef.innerHTML = "";
+
+    for (let i = 0; i < basket.length; i++) {
+        let basketDish = basket[i];
+        let template = /*HTML*/ `
+                        <div class="selectedDish">
+                            <h2 class="mySelectedDish">${basketDish.name}</h2>
+                            <div class="insertedDish">
+                                <form class="portionInformation" onsubmit="calculatePortions(event)">
+                                    <button type="submit" class="buttonMinus">
+                                        <img src="./assets/img/minus.png" alt="Portionen-Icon">
+                                    </button>
+                                    <input id="quantity" type="number" min="1" max="15" step="1" name="quantity"
+                                        required value="${basketDish.amount}">
+                                    <button type="submit" class="buttonPlus">
+                                        <img src="./assets/img/plus.png" alt="Portionen-Icon">
+                                    </button>
+                                </form>
+                                <div class="priceOfTheDish">${basketDish.price.toLocaleString(undefined, {
+                                                                        minimumFractionDigits: 2,
+                                                                        maximumFractionDigits: 2,
+                                                                        style: "currency",
+                                                                        currency: "EUR"
+                                                                    })}</div>
+                                <img class="deleteSelectedDish" src="./assets/img/trash.png"
+                                    alt="imd. Delete your selected dish">
+                            </div>
+                        </div>
+        `
+        basketRef.innerHTML += template;
+    }
+}
+
+function addToBasket(indexDishes) {
 }
